7e2b9c667ebee92ab6df1f1b0ab062964b629c8d
Abbas <muhdabbas98@gmail.com>
2024-06-21 17:59:15 +0800

    fix criteria

--- src/app/components/criteria.tsx
+++ src/app/components/criteria.tsx
@@ -38,25 +38,25 @@ export default function Criteria() {
           Criteria
         </h4>
 
-        <div className="flex items-center justify-center lg:ml-24 lg:px-32">
-          <div className="flex w-96 flex-col items-start gap-3 lg:gap-6">
+        <div className="flex translate-x-3 items-center justify-center lg:translate-x-4 xl:translate-x-8 2xl:ml-24 2xl:px-32">
+          <div className="flex w-96 flex-col items-start gap-3 2xl:gap-6">
             {criterias.slice(0, 3).map((item, index) => (
               <div key={index} className="inline-flex items-center justify-center gap-3 font-light">
-                <span className="flex w-8 justify-center lg:w-14">
+                <span className="flex w-8 justify-center lg:w-10 2xl:w-14">
                   <Image src={item.icon} width={item.width ? item.width : 40} height={40} alt="pallete" />
                 </span>
-                <div className="text-2xl lg:text-4xl">{item.name}</div>
+                <div className="text-2xl lg:text-3xl 2xl:text-4xl">{item.name}</div>
               </div>
             ))}
           </div>
 
-          <div className="flex w-[28rem] flex-col items-start gap-3 lg:gap-6">
+          <div className="flex w-[28rem] flex-col items-start gap-3 2xl:gap-6">
             {criterias.slice(3).map((item, index) => (
               <div key={index} className="inline-flex items-center justify-center gap-3 font-light">
-                <span className="flex w-8 justify-center lg:w-14">
+                <span className="flex w-8 justify-center lg:w-10 2xl:w-14">
                   <Image src={item.icon} width={40} height={40} alt="pallete" />
                 </span>
-                <div className="text-2xl lg:text-4xl">{item.name}</div>
+                <div className="text-2xl lg:text-3xl 2xl:text-4xl">{item.name}</div>
               </div>
             ))}
           </div>
